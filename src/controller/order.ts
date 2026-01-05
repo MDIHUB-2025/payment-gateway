84d9af51a2b3133a0f96de2784e748f359f400f3
Letlhogonolo <164743557+whoIsImai@users.noreply.github.com>
2026-01-05 17:32:57 +0200

    add controller to update order status

--- src/controller/order.ts
+++ src/controller/order.ts
@@ -21,6 +21,18 @@ export async function CreateOrder(req: Request, res: Response): Promise<void> {
   })
   
   res.json({ success: true })
+}
 
+export async function UpdateOrderStatus(req: Request, res: Response): Promise<void> {
 
+    const { orderId } = req.params
+  const { status } = req.body
+
+      const order = orders.get(orderId)
+    if (order) {
+        order.status = status
+        orders.set(orderId, order)
+    }
+  
+  res.json({ success: true })
 }
